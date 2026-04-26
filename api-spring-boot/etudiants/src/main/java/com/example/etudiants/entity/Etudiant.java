065417fc5c77cae4eff277cd9ae2dc06d7c1d109
amal <amalyousef356@gmail.com>
2026-04-26 18:22:10 +0100

    Q2+Q3+Q4 : methode age(), tests BDD Cucumber, index.html

--- api-spring-boot/etudiants/src/main/java/com/example/etudiants/entity/Etudiant.java
+++ api-spring-boot/etudiants/src/main/java/com/example/etudiants/entity/Etudiant.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Data
@@ -20,4 +21,8 @@ public class Etudiant {
     private String cin;
     private String nom;
     private LocalDate dateNaissance;
+
+    public int age() {
+        return Period.between(this.dateNaissance, LocalDate.now()).getYears();
+    }
 }
---
+++ api-spring-boot/etudiants/src/test/java/EtudiantSteps.java
@@ -0,0 +1,29 @@
+package com.example.etudiants;
+
+import com.example.etudiants.entity.Etudiant;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EtudiantSteps {
+
+    private Etudiant etudiant;
+    private int age;
+
+    @Given("un étudiant avec la date de naissance {string}")
+    public void unEtudiantAvecLaDateDeNaissance(String date) {
+        etudiant = new Etudiant(null, "12345678", "Test", LocalDate.parse(date));
+    }
+
+    @When("on calcule son âge")
+    public void onCalculeSonAge() {
+        age = etudiant.age();
+    }
+
+    @Then("l'âge retourné doit être {int}")
+    public void lAgeRetourneDoitEtre(int ageAttendu) {
+        assertEquals(ageAttendu, age);
+    }
+}
